afec32ee1e30d517c61e98c8b7386fd184fb9f76
glubbfreund <oliver.leis@gmail.com>
2024-02-20 20:17:03 +0100

    added player detection

--- scripts/main.ts
+++ scripts/main.ts
@@ -1,7 +1,12 @@
-import { Vector3, world, TargetBlockHitAfterEvent, BlockComponent } from "@minecraft/server";
+import { world, TargetBlockHitAfterEvent } from "@minecraft/server";
 
 function targetBlockHandler(e: TargetBlockHitAfterEvent) {
-  world.sendMessage("§7" + e.redstonePower.toString() + " Punkte erzielt.");
+  let players = e.dimension.getPlayers({
+    location: { x: e.block.x, y: e.block.y - 1, z: e.block.z - 15 },
+    closest: 1,
+    maxDistance: 1,
+  });
+  world.sendMessage("§7" + players[0].nameTag + " hat " + e.redstonePower.toString() + " Punkte erzielt.");
 }
 
 world.afterEvents.targetBlockHit.subscribe(targetBlockHandler);
