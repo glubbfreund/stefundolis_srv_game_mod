246ea764fda9f7d6e48ffca1f5724ef5e2948ca2
glubbfreund <oliver.leis@gmail.com>
2024-02-20 20:58:13 +0100

    added dynamic player detection

--- scripts/main.ts
+++ scripts/main.ts
@@ -1,8 +1,23 @@
-import { world, TargetBlockHitAfterEvent } from "@minecraft/server";
+import { Vector3, world, TargetBlockHitAfterEvent } from "@minecraft/server";
 
 function targetBlockHandler(e: TargetBlockHitAfterEvent) {
+  let ppos: Vector3 = { x: 0, y: 0, z: 0 };
+  switch (e.source.getBlockFromViewDirection()?.face.toString()) {
+    case "North":
+      ppos = { x: e.block.x, y: e.block.y - 1, z: e.block.z - 15 };
+      break;
+    case "South":
+      ppos = { x: e.block.x, y: e.block.y - 1, z: e.block.z + 15 };
+      break;
+    case "West":
+      ppos = { x: e.block.x - 15, y: e.block.y - 1, z: e.block.z };
+      break;
+    case "East":
+      ppos = { x: e.block.x + 15, y: e.block.y - 1, z: e.block.z };
+      break;
+  }
   let players = e.dimension.getPlayers({
-    location: { x: e.block.x, y: e.block.y - 1, z: e.block.z - 15 },
+    location: ppos,
     closest: 1,
     maxDistance: 1,
   });
